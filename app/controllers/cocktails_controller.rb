8609152f3b46bbd01ae93f6c8a13d2ee2fb1fb2f
Bruno Johnson <brunojs91@gmail.com>
2020-04-28 21:36:01 -0300

    js

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,35 @@
 class CocktailsController < ApplicationController
-    def index
+  before_action :set_cocktail, only: :show
+  def index
     @cocktails = Cocktail.all
-    @search = params['search']
-    if @search.present?
-      @name = @search['cocktail_name']
-      @cocktails = Cocktail.where('name ILIKE ?', "%#{@name}%")
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
     end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
+
+  resources :cocktails, only: %i[index new show create] do
+    resources :doses, only: %i[new create destroy]
+    resources :reviews, only: [:create]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
