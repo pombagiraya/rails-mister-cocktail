ae6546ad3d12c1146300630455360299619091d5
Bruno Johnson <brunojs91@gmail.com>
2020-04-30 19:56:30 -0300

    controllers added, but one

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: %i[new create edit destroy]
+  before_action :find_dose, only: %i[edit destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+end
